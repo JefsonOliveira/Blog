43d274df47b04c1590227c897e1339d11b1cf6af
JefsonOliveira <kaironoliveira16@gmail.com>
2024-07-08 15:28:51 -0300

    feat: Ate 6/16 da sessão -Blog- -ReactJs- -Hora De Codar-

--- arquivos_blog/src/main.jsx
+++ arquivos_blog/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // Pages
 import Home from "./routes/Home.jsx";
