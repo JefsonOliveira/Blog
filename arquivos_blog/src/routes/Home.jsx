4310ddad232accc4668f6924ea7fa02118ad6eb4
JefsonOliveira <kaironoliveira16@gmail.com>
2024-07-10 15:13:37 -0300

    feat: Ate 10/16 da sessão -Blog- -ReactJs- -Hora De Codar-

--- arquivos_blog/src/routes/Home.jsx
+++ arquivos_blog/src/routes/Home.jsx
@@ -1,19 +1,17 @@
+import blogFetch from "../axios/config";
+
 import { useState, useEffect } from "react";
 
 import { Link } from "react-router-dom";
 
 import "./Home.css";
 
-import axios from "axios";
-
 const Home = () => {
   const [posts, setPosts] = useState([]);
 
   const getPosts = async () => {
     try {
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
+      const response = await blogFetch.get("/posts");
 
       const data = response.data;
 
@@ -37,7 +35,9 @@ const Home = () => {
           <div className="post" key={post.id}>
             <h2>{post.title}</h2>
             <p>{post.body}</p>
-            <Link to={`/posts/${post.id}`} className="btn">Ler mais</Link>
+            <Link to={`/posts/${post.id}`} className="btn">
+              Ler mais
+            </Link>
           </div>
         ))
       )}
