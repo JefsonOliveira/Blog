f1e4d74b0c8ead1479fcb9db4f86880537c23970
JefsonOliveira <kaironoliveira16@gmail.com>
2024-07-11 15:34:16 -0300

    feat: Conclusão do projeto -Blog- -ReactJs- -HoraDeCodar-

--- arquivos_blog/src/main.jsx
+++ arquivos_blog/src/main.jsx
@@ -8,6 +8,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./routes/Home.jsx";
 import NewPost from "./routes/NewPost.jsx";
 import Post from "./routes/Post.jsx";
+import Admin from "./routes/Admin.jsx";
+import EditPost from "./routes/EditPost.jsx";
 
 import "./index.css";
 
@@ -26,6 +28,14 @@ const router = createBrowserRouter([
       {
         path: "/posts/:id",
         element: <Post />
+      },
+      {
+        path: "/admin",
+        element: <Admin />
+      },
+      {
+        path: "/posts/edit/:id",
+        element: <EditPost />
       }
     ],
   },
